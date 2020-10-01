b334aa3fa889728ab56d6f9959f7814bf634b4f4
ghsoares <ghsoares99795@gmail.com>
2020-10-01 10:09:08 -0300

    cleaned to a new prototype

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
-import BigNumber from 'bignumber.js';
-import Decimal from 'decimal.js';
 import React from 'react';
-import Game from './components/Game';
-import GameProvider from './contexts/GameContext';
-
-Decimal.prototype.neq = function (n = new BigNumber()) {
-  return !this.eq(n);
-};
 
 const App = () => {
-  return (
-    <GameProvider>
-      <Game></Game>
-    </GameProvider>
-  );
+  return null
 }
-
 
 export default App;
